51faaa5ac2b69b7f24a243d23abb065353288a8b
Trevor <mrtj458@gmail.com>
2017-09-11 23:55:50 -0600

    Version 1 Complete.
    Added final comments.

--- Main.py
+++ Main.py
@@ -1,18 +1,27 @@
+# CMDCalc Version 1.
+# Created by, Trevor Hodsdon.
+
 first_number = ""
 operation = ""
 second_number = ""
 
+# Main method for the Program.
 def main():
-    print("CMDCalc Alpha.")
+    # Print info.
+    print("CMDCalc Version 1.")
     print("Type 'help' for a list of commands.")
     print("Type 'quit' to quit.")
+
+    # Get command from the user.
     command = input("Enter a calculation: ")
 
+    # Check what the command is.
     if command.lower() == "help":
         show_commands()
     elif command.lower() == "quit":
         exit()
     else:
+        # Attempt to split the inputted calculation into 3 variables.
         try:
             global first_number
             global operation
@@ -20,7 +29,7 @@ def main():
             first_number, operation, second_number = command.split()
         except ValueError:
             print("Unknown operation.")
-
+    # Check what operation was entered and output the correct calculation.
     if operation == "+":
         print("{} + {} = {:.2} ".format(first_number, second_number, add_numbers()))
     elif operation == "-":
@@ -32,16 +41,21 @@ def main():
     elif operation == "%":
         print("{} % {} = {:.2} ".format(first_number, second_number, remaining_numbers()))
 
+# Adds the numbers together.
 def add_numbers():
     return float(first_number) + float(second_number)
 
+# Subtract the numbers.
 def subtract_numbers():
     return float(first_number) - float(second_number)
 
+# Multiply the numbers.
 def multiply_numbers():
     return float(first_number) * float(second_number)
 
+# Devide the numbers.
 def divide_numbers():
+    # Try to divide the numbers and catch any 'divide by zero' exceptions.
     try:
         answer = float(first_number) / float(second_number)
         return answer
@@ -49,9 +63,11 @@ def divide_numbers():
         print("Can't divide by zero.")
         return 0.0
 
+# Get the remainder of the numbers.
 def remaining_numbers():
     return float(first_number) % float(second_number)
 
+# Prints out information and available commands to the console.
 def show_commands():
     print()
     print("Format your questions in the following way")
@@ -65,6 +81,7 @@ def show_commands():
     print("Divide: /")
     print("Modulus: %")
 
+# Loop the main method forever.
 while True:
     main()
     print()
